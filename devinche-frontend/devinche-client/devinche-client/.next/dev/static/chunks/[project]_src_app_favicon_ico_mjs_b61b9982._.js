daa950f1b6d90ca25c79044175f2c53860bc8cb9
Rocket Primm <primmrock@gmail.com>
2026-02-22 14:26:07 +0100

    fix: remove stray devinche-client folder

--- devinche-frontend/devinche-client/devinche-client/.next/dev/static/chunks/[project]_src_app_favicon_ico_mjs_b61b9982._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/b7907_next_dist_7c99dd79._.js"
-],
-    source: "dynamic"
-});
